934dc84df533a78a1e23308ad2854ac4c9ab1e9c
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-07-04 13:53:56 +0200

    fix stock in FlowerShop

--- src/main/java/Model/FlowerShop.java
+++ src/main/java/Model/FlowerShop.java
@@ -1,6 +1,7 @@
 package Model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FlowerShop {
 
@@ -8,71 +9,74 @@ public class FlowerShop {
     private int id;
     private static int nextId = 1;
     private String name;
-    private ArrayList<Product> stock;
+    private Map<Product, Integer> stock;
     private double stockValue;
 
     // Constructor
-    public FlowerShop (String name) {
+    public FlowerShop(String name) {
         this.id = nextId++;
         this.name = name;
-        this.stock = new ArrayList<>();
+        this.stock = new HashMap<>();
         this.stockValue = totalValue();
     }
 
     // Getters
-    public int getId(){
+    public int getId() {
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
+
     public double getStockValue() {
         return stockValue;
     }
 
-    public ArrayList<Product> getStock() {
+    public Map<Product, Integer> getStock() {
         return stock;
     }
 
     // Setters
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
     // General methods
-    public void showStock(){
+    public void showStock() {
         int decorationStock = 0;
         int flowerStock = 0;
         int treeStock = 0;
 
-        if (stock.isEmpty()){
+        if (stock.isEmpty()) {
             System.out.println("The stock is empty");
         } else {
-            verifyStock(decorationStock,flowerStock,treeStock);
+            verifyStock(decorationStock, flowerStock, treeStock);
             System.out.println("Decorations : " + decorationStock +
                     "\nFlowers : " + flowerStock +
                     "\nTrees : " + treeStock);
         }
     }
 
-    public void verifyStock(int decorationStock, int flowerStock, int treeStock){
-        for (Product product : stock){
-            if (product.getClass() == Decoration.class){
-                decorationStock++;
+    public void verifyStock(int decorationStock, int flowerStock, int treeStock) {
+        for (Map.Entry<Product, Integer> entry : stock.entrySet()) {
+            Product product = entry.getKey();
+            if (product.getClass() == Decoration.class) {
+                decorationStock += entry.getValue();
             } else if (product.getClass() == Flower.class) {
-                flowerStock++;
+                flowerStock += entry.getValue();
             } else if (product.getClass() == Tree.class) {
-                treeStock++;
+                treeStock += entry.getValue();
             }
         }
     }
 
-    public double totalValue(){
+    public double totalValue() {
         double totalValue = 0d;
 
-        if (!stock.isEmpty()){
-            for (Product product : stock) {
-                totalValue += product.getPrice();
+        if (!stock.isEmpty()) {
+            for (Map.Entry<Product, Integer> entry : stock.entrySet()) {
+                totalValue += entry.getKey().getPrice() * entry.getValue();
             }
         }
         return totalValue;
